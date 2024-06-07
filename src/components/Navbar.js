6c2cd6f8ecd6a13087e6a198d4e593530d6db955
Manavi Sharma <150844157+manavi-24@users.noreply.github.com>
2024-06-07 19:42:58 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,20 @@ export default function Navbar(props) {
     //<nav className="navbar navbar-expand-lg bg-body-tertiary">
     <nav className={`navbar bg-${props.mode} border-bottom border-body navbar-expand-lg bg-body-tertiary`} data-bs-theme={`${props.mode}`}>
     <div className="container-fluid">
-      {/*<Link className="navbar-brand" to="/home">{props.title}</Link>*/}
-      <a className="navbar-brand" href="#home">{props.title}</a>
+      <Link className="navbar-brand" to="/home">{props.title}</Link>
+      {/*<a className="navbar-brand" href="#home">{props.title}</a>*/}
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            {/*<Link className="nav-link" aria-current="page" to="/home">Home</Link>*/}
-            <a className="nav-link" aria-current="page" href="#home">Home</a>
+            <Link className="nav-link" aria-current="page" to="/home">Home</Link>
+            {/*<a className="nav-link" aria-current="page" href="#home">Home</a>*/}
           </li>
           <li className="nav-item">
-            {/*<Link className="nav-link" to="/about">{props.abouttext}</Link>*/}
-            <a className="nav-link" href="#about">{props.abouttext}</a>
+            <Link className="nav-link" to="/about">{props.abouttext}</Link>
+            {/*<a className="nav-link" href="#about">{props.abouttext}</a>*/}
           </li>
         </ul>
         <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
